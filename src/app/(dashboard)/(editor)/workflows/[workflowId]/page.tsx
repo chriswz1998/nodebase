f554c6aad7926efd91c42e143cf2c725fd11fd4a
chris.wz <scissorwz@gmail.com>
2025-11-17 03:08:59 -0800

    09: sidebar layout

---
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -0,0 +1,14 @@
+import {requireAuth} from "@/lib/auth-utils";
+
+interface PageProps {
+    params: Promise<{workflowId: String}>
+}
+
+const Page = async ({params}: PageProps) => {
+    await requireAuth()
+    const {workflowId} = await params
+
+    return <p>workflow id: {workflowId}</p>
+}
+
+export default Page
---
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,14 @@
+import {requireAuth} from "@/lib/auth-utils";
+
+interface PageProps {
+    params: Promise<{credentialId: String}>
+}
+
+const Page = async ({params}: PageProps) => {
+    await requireAuth()
+    const {credentialId} = await params
+
+    return <p>Credentials id: {credentialId}</p>
+}
+
+export default Page
