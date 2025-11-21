a8c1a2f9941964e87c07ed1ccc6e2d1831ec3555
chris.wz <scissorwz@gmail.com>
2025-11-21 00:37:25 -0800

    12: workflows pagination

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,18 +1,33 @@
 'use client'
 
 import {useCreateWorkflow, useSuspenseWorkflows} from "@/features/workflows/hooks/use-workflows";
-import {EntityContainer, EntityHeader} from "@/components/entity-components";
+import {EntityContainer, EntityHeader, EntityPagination, EntitySearch} from "@/components/entity-components";
 import {ReactNode} from "react";
 import {Workflow} from "@/generated/prisma/client";
 import {useUpgradeModal} from "@/hooks/use-upgrade-modal";
 import {useRouter} from "next/navigation";
+import {useWorkflowsParams} from "@/features/workflows/hooks/use-workflows-params";
+import {useEntitySearch} from "@/hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+    const [params, setParams] = useWorkflowsParams()
+    const {searchValue, onSearchChange} = useEntitySearch({params,setParams})
+
+    return (
+        <EntitySearch
+            value={searchValue}
+            onChange={onSearchChange}
+            placeholder={'Search workflows'}
+        />
+    )
+}
 
 export const WorkflowsList = () => {
     const workflows = useSuspenseWorkflows()
 
     return (
-        <div className={'flex-1 flex justify-center items-center'}>
-            <p>{JSON.stringify(workflows.data)}</p>
+        <div className={'flex-1 flex items-center justify-center'}>
+            <p>{JSON.stringify(workflows.data, null, 2)}</p>
         </div>
     )
 }
@@ -48,12 +63,26 @@ export const WorkflowsHeader = ({disabled}: {disabled?: boolean}) => {
     )
 }
 
+export const WorkflowsPagination = () => {
+    const [params, setParams] = useWorkflowsParams()
+    const workflows = useSuspenseWorkflows()
+
+    return (
+        <EntityPagination
+            disable={workflows.isFetching}
+            page={workflows.data.page}
+            totalPages={workflows.data.totalPages}
+            onPageChange={(page) => setParams({...params,page})}
+        />
+    )
+}
+
 export const WorkflowsContainer = ({children}: {children: ReactNode}) => {
     return (
         <EntityContainer
             header={<WorkflowsHeader/>}
-            search={<></>}
-            pagination={<></>}
+            search={<WorkflowsSearch/>}
+            pagination={<WorkflowsPagination/>}
         >
             {children}
         </EntityContainer>
