e1faf5e1d7ac9d755b90c98dcf853401254e5f4e
chris.wz <scissorwz@gmail.com>
2025-12-04 15:48:25 -0800

    24: Ai nodes

---
+++ src/features/executions/components/openai/node.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import {Node, NodeProps, useReactFlow} from "@xyflow/react";
+import {memo, useState} from "react";
+import {BaseExecutionNode} from "@/features/executions/components/base-execution-node";
+import {useNodeStatus} from "@/features/executions/hooks/use-node-states";
+import {OpenAIDialog, OpenAIFormValues} from "@/features/executions/components/openai/dialog";
+import {fetchOpenAIRealtimeToken} from "@/features/executions/components/openai/actions";
+import {OPENAI_CHANNEL_NAME} from "@/inngest/channels/openai";
+
+type OpenAINodeData = {
+     variableName?: string;
+     systemPrompt?: string;
+     userPrompt?: string;
+}
+
+type OpenAINodeType = Node<OpenAINodeData>
+
+export const OpenAINode = memo((props: NodeProps<OpenAINodeType>) => {
+    const [dialogOpen, setDialogOpen] = useState(false)
+    const {setNodes} = useReactFlow()
+
+    const handleOpenSettings = () => setDialogOpen(true)
+
+    const handleSubmit = (value: OpenAIFormValues) => {
+        setNodes((nodes) => nodes.map((node => {
+            if (node.id === props.id) {
+                return {
+                    ...node,
+                    data: {
+                        ...node.data,
+                        ...value
+                    }
+                }
+            }
+            return node
+        })))
+    }
+
+    const nodeData = props.data
+    const description = nodeData?.userPrompt
+        ? `"gpt-5-nano" : ${nodeData.userPrompt.slice(0,50)}...`
+        : 'Not configured'
+
+    const nodeStatus = useNodeStatus({
+        nodeId: props.id,
+        channel: OPENAI_CHANNEL_NAME,
+        topic: "status",
+        refreshToken: fetchOpenAIRealtimeToken
+    })
+
+    return (
+        <>
+            <OpenAIDialog
+                onSubmit={handleSubmit}
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+                defaultValues={nodeData}
+            />
+            <BaseExecutionNode
+                {...props}
+                id={props.id}
+                icon={'/openai.svg'}
+                name={'OpenAI'}
+                status={nodeStatus}
+                description={description}
+                onSettings={handleOpenSettings}
+                onDoubleClick={handleOpenSettings}
+            />
+        </>
+    )
+})
+
+OpenAINode.displayName = "OpenAINode";
--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -4,6 +4,8 @@ import {manualTriggerExecutor} from "@/features/triggers/components/manual-trigg
 import {httpRequestExecutor} from "@/features/executions/components/http-request/executor";
 import {googleFormExecutor} from "@/features/triggers/components/google-form-trigger/executor";
 import {StripeTriggerExecutor} from "@/features/triggers/components/stripe-trigger/executor";
+import {GeminiExecutor} from "@/features/executions/components/gemini/executor";
+import {OpenAIExecutor} from "@/features/executions/components/openai/executor";
 
 
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
@@ -11,7 +13,9 @@ export const executorRegistry: Record<NodeType, NodeExecutor> = {
     [NodeType.INITIAL]: manualTriggerExecutor,
     [NodeType.HTTP_REQUEST]: httpRequestExecutor,
     [NodeType.GOOGLE_FORM_TRIGGER]: googleFormExecutor,
-    [NodeType.STRIPE_TRIGGER]: StripeTriggerExecutor
+    [NodeType.STRIPE_TRIGGER]: StripeTriggerExecutor,
+    [NodeType.GEMINI]: GeminiExecutor,
+    [NodeType.OPENAI]: OpenAIExecutor
 }
 
 export const getExecutor = (type: NodeType): NodeExecutor => {
